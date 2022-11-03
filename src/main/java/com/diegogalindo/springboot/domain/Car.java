13844951ba0e253ad559c412f8fa51f820f981b9
Diego Galindo <diegogalindo84@gmail.com>
2022-11-03 15:20:23 -0300

    Banco MySQL configurado

---
+++ src/main/java/com/diegogalindo/springboot/domain/Car.java
@@ -0,0 +1,55 @@
+package com.diegogalindo.springboot.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    private String model;
+
+
+    private String color;
+
+
+    private int year;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/test/java/com/diegogalindo/aluguelveiculos/AluguelVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegogalindo.aluguelveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelVeiculosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
